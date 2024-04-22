c15b73365b1c429145a27d2cf08b3709d6a4a551
derikaquary <derikaquary@gmail.com>
2024-04-22 12:04:17 +0800

    deleted build from vite config

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,6 @@ import eslint from "vite-plugin-eslint";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
-  build: {
-    outDir: "build",
-  },
   server: {
     proxy: {
       "/api": {
