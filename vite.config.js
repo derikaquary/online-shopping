adfaea3edbed4004b1587fca52a66c84a9e09fa0
derikaquary <derikaquary@gmail.com>
2024-04-22 09:11:59 +0800

    updating vite config and package json to add server n deliveryOption

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/online-shopping/",
+  plugins: [react(), eslint()],
+  build: {
+    outDir: "build",
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000", // Assuming your API server is running at this address
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""), // Remove the '/api' prefix
+      },
+      "/delivery": {
+        target: "http://localhost:8001", // Assuming your delivery options server is running at this address
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/delivery/, ""), // Remove the '/delivery' prefix
+      },
+    },
+  },
 });
