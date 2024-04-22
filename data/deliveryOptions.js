0145bc610f327e37290950776e1787ca6ef8ac28
derikaquary <derikaquary@gmail.com>
2024-04-22 10:35:29 +0800

    copy data

---
+++ data/deliveryOptions.js
@@ -0,0 +1,20 @@
+export const deliveryOptionsApi = [
+  {
+    id: "1",
+    deliveryDays: 7,
+    priceCents: 0,
+    tag: " Free Shipping",
+  },
+  {
+    id: "2",
+    deliveryDays: 3,
+    priceCents: 499,
+    tag: " - Shipping",
+  },
+  {
+    id: "3",
+    deliveryDays: 1,
+    priceCents: 999,
+    tag: " - Shipping",
+  },
+];
